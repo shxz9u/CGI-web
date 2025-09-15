2755b3a436f2c3c4c451c49180844f3a46cce860
YSJ0620 <qpxjfdl@naver.com>
2025-09-16 08:40:46 +0900

    added index.html, fixed route

--- cgi-src/dashboard.c
+++ cgi-src/dashboard.c
@@ -1,12 +1,9 @@
 ﻿#include <stdio.h>
+
 int main(void)
 {
     printf("Content-Type: text/html; charset=utf-8\r\n\r\n");
     printf("<h1>대시보드</h1>");
-    printf("<ul>");
-    printf("<li><a href=\"/web-cgi/ipdiag.html\">IP 진단</a></li>");
-    printf("<li><a href=\"/web-cgi/box.html\">프린터 박스</a></li>");
-    printf("<li><a href=\"/web-cgi/print.html\">출력 기능</a></li>");
-    printf("</ul>");
+    printf("<p>여기는 프린터 관리 현황 요약을 보여주는 CGI 페이지입니다.</p>");
     return 0;
 }
